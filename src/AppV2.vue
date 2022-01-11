fa2878771c5fad22f58ecba466801236e656115f
Viktors <viktors97n@gmail.com>
2022-01-12 00:18:17 +0200

    Add required packages, setup simple App components

--- src/AppV2.vue
+++ src/AppV2.vue
@@ -1,32 +1,34 @@
 <template>
-  <h3>Test using wallet-provider component</h3>
+  <h3>Test using "global store" component</h3>
 
-  <wallet-provider :auto-connect="true" :wallets="wallets">
-    <wallet-modal-provider>
-      <wallet-multi-button/>
-    </wallet-modal-provider>
-  </wallet-provider>
+  <wallet-modal-provider>
+    <wallet-multi-button/>
+  </wallet-modal-provider>
 </template>
 
 <script lang="ts">
   import {defineComponent} from 'vue';
   import {Wallet} from "@solana/wallet-adapter-base";
-  import {WalletProvider} from "@solana/wallet-adapter-vue";
+  import {initWallet} from "@solana/wallet-adapter-vue";
   import {getPhantomWallet, getSolflareWallet} from "@solana/wallet-adapter-wallets";
   import {WalletModalProvider, WalletMultiButton} from "@solana/wallet-adapter-vue-ui";
 
   export default defineComponent({
-    name: 'App',
+    name: 'AppV2',
     components: {
       WalletMultiButton,
       WalletModalProvider,
-      WalletProvider,
     },
     setup() {
       const wallets: Wallet[] = [
         getPhantomWallet(),
         getSolflareWallet(),
       ];
+
+      initWallet({
+        wallets,
+        autoConnect: true,
+      });
 
       return {wallets};
     }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+import AppV2 from "@/AppV2.vue";
+
+// createApp(App).mount('#app');
+createApp(AppV2).mount('#app');
